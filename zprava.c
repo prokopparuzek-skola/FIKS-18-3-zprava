bf8d96d470a691b7e994839bcdcf41b605bb92a4
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-03-31 15:51:08 +0200

    0.1 mozna dokonceno

--- zprava.c
+++ zprava.c
@@ -2,13 +2,10 @@
 #include <stdlib.h>
 #include<string.h>
 #define RESET_PAR for (j = 0; j < n; j++) queue.parents[j] = -1
-#define IMEST 0 // indexovani mest v zadani(1 od 1; 0 od 0)
 
 typedef int edge; // uchovává hrany grafu
 
 typedef struct {
-    int a;
-    int b;
     int indexAc;
     int indexFu;
     unsigned *stackAc;
@@ -16,60 +13,41 @@ typedef struct {
     int *parents;
 } buffer_t; // buffer se všemi proměnými potřebnými pro hledání v grafu
 
-int makeSteps(edge **graph, buffer_t *queue, int max);
-int solveStep(edge **graph, buffer_t *queue, int index, int b);
+int makeSteps(edge **graph, buffer_t *queue, int max, int*, int);
+int solveStep(edge **graph, buffer_t *queue, int index, int, int*, int);
 void addEdge(edge *vertex, int to);
-void initGraph(edge **graph, int n);
-void findRoute(edge ** graph, buffer_t *queue, int n);
-int solve(edge **graph, buffer_t *queue, int max);
-void bts(edge **graph, int n, int a, int b, int maxCount);
+void initGraph(edge *graph, int d);
+int solve(edge **graph, buffer_t *queue, int max, int*, int);
+void bts(edge **graph, int n, int maxCount, int*, int);
+int isIn(int centr, int* attacked, int from, int to);
 
 void addEdge(edge *vertex, int to) {
     vertex[vertex[0]] = to;
     vertex[0]++;
 }
 
-void initGraph(edge **graph, int n) {
-    int i, j;
+void initGraph(edge *graph, int d) {
+    int i;
 
-    for (i = 0; i < n; i++) {
-        graph[i] = malloc((n + 1) * sizeof(edge));
-        graph[i][0] = 0;
-        for (j = 1; j <= n ; j++) {
-            graph[i][j] = -1;
-        }
+    graph = malloc((d + 1) * sizeof(edge));
+    graph[0] = 0;
+    for (i = 1; i <= d ; i++) {
+        graph[i] = -1;
     }
 }
 
-void findRoute(edge ** graph, buffer_t *queue, int n) {
-    int position = queue->b;
-    int route[n];
-    int *index = route;
-
-    while (position != queue->a) { // reverzně prochází cestu
-        *index++ = position;
-        position = queue->parents[position];
-    }
-    *index = queue->a;
-    while (index != route) { // otočí pořadí měst a vypíše je
-        printf("%d ", *index + IMEST);
-        index--;
-    }
-    printf("%d\n", *index + IMEST); // doplní cílové město
-}
-
-int solve(edge **graph, buffer_t *queue, int max) {
+int solve(edge **graph, buffer_t *queue, int max, int *attacked, int k) {
     int back = 1;
     while (back == 1) {
-        back = makeSteps(graph, queue, max); // volá hledání pro 1 sadu vrcholů
+        back = makeSteps(graph, queue, max, attacked, k); // volá hledání pro 1 sadu vrcholů
     }
     return back;
 }
 
-void bts(edge **graph, int n, int a, int b, int maxCount) { // provádí hledání v grafu
+void bts(edge **graph, int n, int maxCount, int *attacked, int k) { // provádí hledání v grafu
     int i, j;
     int back; // návratová hodnota solveStep
-    buffer_t queue = {a, b, 0, -1, NULL, NULL, NULL}; // indexFu je -1, před 1. použitím se zvětší
+    buffer_t queue = {0, -1, NULL, NULL, NULL}; // indexFu je -1, před 1. použitím se zvětší
 
     queue.stackAc = malloc(n * sizeof(unsigned));
     queue.stackFu = malloc(n * sizeof(unsigned));
@@ -81,18 +59,16 @@ void bts(edge **graph, int n, int a, int b, int maxCount) { // provádí hledán
 
     for (i = 0;; i++) {
         RESET_PAR;              // inicializuje queue před hledáním
-        queue.stackAc[0] = a;
+        queue.stackAc[0] = 0;
         queue.indexAc = 0;
         queue.indexFu = -1;
-        queue.parents[a] = a;
-        back = solve(graph, &queue, i); // hledá dokud nedosáhne konce, nebo jí nedojdou volné vrcholy
+        queue.parents[0] = 0;
+        back = solve(graph, &queue, i, attacked, k); // hledá dokud nedosáhne konce, nebo jí nedojdou volné vrcholy
         if (back == 0) { // jsi u cíle, vypiš cestu
-            findRoute(graph, &queue, n);
             printf("%d\n", i); // vypiš maximální počet hlídek
             break;
         }
         if (i == maxCount && back == 2) { // nic jsi nenašel, vypiš nesmysly
-            printf("1 2 3\n");
             printf("-1\n");
         }
     }
@@ -101,12 +77,12 @@ void bts(edge **graph, int n, int a, int b, int maxCount) { // provádí hledán
     free(queue.parents);
 }
 
-int makeSteps(edge **graph, buffer_t *queue, int max) {
+int makeSteps(edge **graph, buffer_t *queue, int max, int* attacked, int k) {
     unsigned int i, *swapS, test = 0;
     int back; // návratová hodnota solveStep
 
     for (i = 0; i <= queue->indexAc; i++) {
-        back = solveStep(graph, queue, i, max); // hledá všechny možné cesty pro 1 vrchol grafu
+        back = solveStep(graph, queue, i, max, attacked, k); // hledá všechny možné cesty pro 1 vrchol grafu
         if (back == 0) { // jsi u cíle, dej vědět
             return 0;
         }
@@ -125,15 +101,33 @@ int makeSteps(edge **graph, buffer_t *queue, int max) {
     return 2; // když ne, dá vědět
 }
 
-int solveStep(edge **graph, buffer_t *queue, int index, int max) {
+int isIn(int centr, int* attacked, int from, int to) {
+    int hub = (from + to)/2;
+    if (attacked[hub] == centr) {
+        return 0;
+    }
+    else if (from != hub) {
+        if (attacked[hub] > centr) {
+            return isIn(centr, attacked, from, hub);
+        }
+        else {
+            return isIn(centr, attacked, hub, to);
+        }
+    }
+    else {
+        return 1;
+    }
+}
+
+int solveStep(edge **graph, buffer_t *queue, int index, int max, int *attacked, int k) {
     int i, test = 0;
-    for (i = 0; graph[queue->stackAc[index]][i] != -1; i++) { // projde všechny hrany z daného vrcholu
-        if (queue->parents[graph[queue->stackAc[index]][i]] == -1) { // Je dostatečně malý počet hlídek a nebyl jsem tu?
+    for (i = 0; i <= max; i++) { // projde všechny hrany z daného vrcholu
+        if (queue->parents[graph[queue->stackAc[index]][i]] == -1) { // Nebyl jsem tu?
             test = 1;
             queue->indexFu++; // zvyš index příštích bodů o 1
             queue->stackFu[queue->indexFu] = graph[queue->stackAc[index]][i]; // ulož tam aktuální bod
             queue->parents[graph[queue->stackAc[index]][i]] = queue->stackAc[queue->indexAc]; // ulož odkud jsi vyšel, nutné pro rekonstrukci cesty
-            if (graph[queue->stackAc[index]][i] == queue->b) { // jsi u cíle, dej vědět
+            if (isIn(graph[queue->stackAc[index]][i], attacked, 0, k)) { // jsi u cíle, dej vědět
                 return 0;
             }
         }
@@ -141,28 +135,41 @@ int solveStep(edge **graph, buffer_t *queue, int index, int max) {
     return test?1:2; // ne dej vědět zda jsi něco našel(1), nebo ne(2)
 }
 
+int cmp(const void *a, const void *b) {
+    int* x = (int*)a;
+    int* y = (int*)b;
+    return *x - *y;
+}
+
 int main() {
-    int t, n, k, a, b;
+    int t, n, k, d, to;
     int maxCount = 0;
     edge **graph;
-    int i;
+    int i, j;
 
     scanf("%d",&t);
     for (; t > 0; t--) {
         scanf("%d%d", &n, &k);
+
         int attacked[k];
+        graph = malloc(n * sizeof(edge*));
+
         for (i = 0; i < k; i++) {
             scanf("%d", &attacked[i]);
         }
         for (i = 0; i < n; i++) { // načte graf
-            graph = malloc(n * sizeof(edge*));
-            initGraph(graph, n);
-            scanf("%d%d", &a, &b);
-            a -= IMEST;
-            b -= IMEST;
-            addEdge(graph[a], b);
+            scanf("%d", &d);
+            initGraph(graph[i], d);
+            for (j = 0; j < d; j++) {
+                scanf("%d", &to);
+                addEdge(graph[i], to);
+                if (j > maxCount) {
+                    maxCount = j;
+                }
+            }
         }
-        bts(graph, n, s, c,maxCount);
+        qsort(attacked, k, sizeof(int), cmp);
+        bts(graph, n, maxCount, attacked, k);
 
         for (n--; n >= 0; n--) {
             free(graph[n]);
